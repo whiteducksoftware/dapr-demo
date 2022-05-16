bdb55b21b155156c3f0c8b039c188b2331648ba4
Martin Brandl <mjibrandl@gmail.com>
2022-05-16 08:50:50 +0200

    add dapr output

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -6,6 +6,9 @@ az deployment group create -g $rgName -f main.bicep
 
 # az containerapp revision show -n 'messageservice' -g 'rg-community-build-conapp' --revision messageservice--cjp66ow
 
+# az containerapp env dapr-component list -g rg-community-build-conapp --name env-ozr2nmuwuplzu
+# az containerapp env dapr-component show -g rg-community-build-conapp --name env-ozr2nmuwuplzu --dapr-component-name dapr-demo-state-store
+
 # \
 #   -p \
 #     minReplicas=0 \
