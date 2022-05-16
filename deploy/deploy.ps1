bc84c9c18ba5b32db5d307d372cd1bd920c8b594
Martin Brandl <mjibrandl@gmail.com>
2022-05-16 19:34:17 +0200

    test matrix

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -8,3 +8,39 @@ az deployment group create -g $rgName -f main.bicep
 
 # az containerapp env dapr-component list -g rg-community-build-conapp --name env-ozr2nmuwuplzu
 # az containerapp env dapr-component show -g rg-community-build-conapp --name env-ozr2nmuwuplzu --dapr-component-name dapr-demo-state-store
+
+### Failed to provision revision for container app 'client'. Error details: Operation expired.
+# configuration: {
+#   dapr: {
+#     enabled: true
+#     appId: 'clientdapr'
+#     appProtocol: 'grpc'
+#     appPort: 50001
+#   }
+# }
+
+
+### Provision Status Failed
+# configuration: {
+#     ingress: {
+#     external: false
+#     targetPort: 50001
+#     }
+#     dapr: {
+#     enabled: true
+#     appId: 'clientdapr'
+#     appProtocol: 'grpc'
+#     appPort: 50001
+#     }
+
+### Provision Status Failed
+# configuration: {
+#     ingress: {
+#     external: false
+#     targetPort: 50001
+#     }
+#     }
+
+## NO ingress, NO dapr => WORKING | NO ingress, NO dapr => NOT WORKING
+## NO ingress, NO dapr => WORKING | YES ingress, YES dapr => NOT WORKING (PROVISIONING failed)
+## NO ingress, YES dapr => NOT WOKRING, No revision
