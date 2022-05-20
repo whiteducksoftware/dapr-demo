8b5ea5c792335b63315f6e38cee56830c8289955
Martin Brandl <mjibrandl@gmail.com>
2022-05-20 12:10:58 +0200

    removed unused usings

--- services/messageservice/Services/MessageService.cs
+++ services/messageservice/Services/MessageService.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Dapr.AppCallback.Autogen.Grpc.v1;
